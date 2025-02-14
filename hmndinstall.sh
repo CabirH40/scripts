d107035b7c51aee26e9cfdd3c732cb45be3b86a9
cabir hasan <104315776+CabirH40@users.noreply.github.com>
2025-02-14 18:12:46 +0300

    Update hmndinstall.sh

--- hmndinstall.sh
+++ hmndinstall.sh
@@ -4,39 +4,38 @@
 WORKSPACE_PATH=~/.humanode/workspaces/default
 
 # التحقق من وجود المحفظة
-if [ -f "$WORKSPACE_PATH/key" ]; then
-  echo "هل لديك محفظة؟ (نعم/لا)"
-  read answer
-  if [[ "$answer" == "نعم" || "$answer" == "نعم" ]]; then
-    # إذا كانت الإجابة نعم، يطلب إدخال الكلمات الـ12 واسم النود
-    echo "أدخل الـ 12 كلمة الخاصة بك (مسافة بين الكلمات):"
-    read mnemonic
-    echo "أدخل اسم النود الخاص بك:"
-    read nodename
+echo "هل لديك محفظة؟ (نعم/لا)"
+read answer
 
-    # التبديل إلى المسار الصحيح
-    cd $WORKSPACE_PATH
+if [[ "$answer" == "نعم" || "$answer" == "نعم" ]]; then
+  # إذا كانت الإجابة نعم، يطلب إدخال الكلمات الـ12 واسم النود
+  echo "أدخل الـ 12 كلمة الخاصة بك (مسافة بين الكلمات):"
+  read mnemonic
+  echo "أدخل اسم النود الخاص بك:"
+  read nodename
 
-    # تنفيذ الأمر الخاص بإدخال المفتاح
-    ./humanode-peer key insert --key-type kbai --scheme sr25519 --suri "$mnemonic" --base-path substrate-data --chain chainspec.json
+  # التبديل إلى المسار الصحيح
+  cd $WORKSPACE_PATH
 
-    # تعديل ملف workspace.json باستخدام sed
-    sed -i "s/\"mnemonicInserted\": false/\"mnemonicInserted\": true/" $WORKSPACE_PATH/workspace.json
-    sed -i "s/\"nodename\": \"\"/\"nodename\": \"$nodename\"/" $WORKSPACE_PATH/workspace.json
+  # تنفيذ الأمر الخاص بإدخال المفتاح
+  ./humanode-peer key insert --key-type kbai --scheme sr25519 --suri "$mnemonic" --base-path substrate-data --chain chainspec.json
 
-    # تثبيت الأدوات المطلوبة
-    sudo apt-get install aria2 -y
-    sudo apt install pigz -y
+  # تعديل ملف workspace.json باستخدام sed
+  sed -i "s/\"mnemonicInserted\": false/\"mnemonicInserted\": true/" $WORKSPACE_PATH/workspace.json
+  sed -i "s/\"nodename\": \"\"/\"nodename\": \"$nodename\"/" $WORKSPACE_PATH/workspace.json
 
-    # إزالة البيانات القديمة
-    rm -rf $WORKSPACE_PATH/substrate-data/chains/humanode_mainnet/db/full
+  # تثبيت الأدوات المطلوبة
+  sudo apt-get install aria2 -y
+  sudo apt install pigz -y
 
-    # تحميل البيانات من المصدر
-    aria2c -x 16 -s 16 -o full.tar.gz http://89.116.25.136/24.01.2025/snapshot.tar.gz
-    pigz -dc full.tar.gz | tar -x -C $WORKSPACE_PATH/substrate-data/chains/humanode_mainnet/db/
+  # إزالة البيانات القديمة
+  rm -rf $WORKSPACE_PATH/substrate-data/chains/humanode_mainnet/db/full
 
-    echo "تمت العملية بنجاح!"
-  fi
+  # تحميل البيانات من المصدر
+  aria2c -x 16 -s 16 -o full.tar.gz http://89.116.25.136/24.01.2025/snapshot.tar.gz
+  pigz -dc full.tar.gz | tar -x -C $WORKSPACE_PATH/substrate-data/chains/humanode_mainnet/db/
+
+  echo "تمت العملية بنجاح!"
 else
   # إذا لم تكن هناك محفظة، يقوم بتوليد محفظة جديدة
   echo "ليس لديك محفظة، سيتم توليد واحدة جديدة."
