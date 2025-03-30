281e8440dc90290cad9f60cb3114c057b8635588
cabir hasan <104315776+CabirH40@users.noreply.github.com>
2025-03-30 04:58:22 +0300

    Update whatsbot.sh

--- whatsbot.sh
+++ whatsbot.sh
@@ -6,15 +6,15 @@ apt update -y
 # تثبيت Python 3 و pip3 إذا لم يكن مثبتًا
 apt install python3 python3-pip -y
 
-# تثبيت مكتبات paramiko و schedule
-pip3 install paramiko schedule
+# تثبيت مكتبات paramiko, schedule, requests و pytz
+pip3 install paramiko schedule requests pytz
 
 # إفراغ ملف logs.txt
-> .humanode/workspaces/default/node/logs.txt
+> /root/.humanode/workspaces/default/node/logs.txt
 
 # تنفيذ الأمر لإنشاء جلسة screen وتشغيل السكريبت
 screen -dmS whatsbot
 screen -S whatsbot -X stuff $'wget https://raw.githubusercontent.com/CabirH40/script.sh/main/whatsbot.py -O whatsbot.py && python3 whatsbot.py\n'
 
-# إنهاء السكريبت بعد تنفيذ الأوامر
+# إنهاء السكربت بعد تنفيذ الأوامر
 exit
