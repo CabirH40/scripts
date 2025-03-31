29823cf9a8e02afd0ace350785229c482289635f
cabir hasan <104315776+CabirH40@users.noreply.github.com>
2025-03-31 14:22:38 +0300

    Update whatsbot.sh

--- whatsbot.sh
+++ whatsbot.sh
@@ -18,3 +18,10 @@ screen -S whatsbot -X stuff $'wget https://raw.githubusercontent.com/CabirH40/sc
 
 # إنهاء السكربت بعد تنفيذ الأوامر
 exit
+#for error solvied 
+#sudo apt remove --purge python3 python3-pip python3-venv python3-setuptools python3-wheel -y
+#sudo apt autoremove -y
+#sudo rm -rf /usr/lib/python3* /usr/local/lib/python3* ~/.local/lib/python3* ~/.cache/pip
+#sudo apt update && sudo apt install python3 python3-pip python3-venv python3-setuptools python3-wheel libssl-dev -y
+#pip3 install --no-cache-dir requests urllib3 cryptography pyOpenSSL paramiko schedule pytz
+#python3 whatsbot.py
