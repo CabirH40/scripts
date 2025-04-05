0fe95bb9bd53dfb6185844950b82b5cc3117f615
cabir hasan <104315776+CabirH40@users.noreply.github.com>
2025-04-05 16:50:59 +0300

    Update whatsbotPythonayarlar.sh

--- whatsbotPythonayarlar.sh
+++ whatsbotPythonayarlar.sh
@@ -12,9 +12,6 @@ pip3 install paramiko schedule requests pytz
 # إفراغ ملف logs.txt
 > /root/.humanode/workspaces/default/node/logs.txt
 
-# تنفيذ الأمر لإنشاء جلسة screen وتشغيل السكريبت
-screen -dmS whatsbot
-screen -S whatsbot -X stuff $'wget https://raw.githubusercontent.com/CabirH40/script.sh/main/whatsbot.py -O whatsbot.py && python3 whatsbot.py\n'
 
 # إنهاء السكربت بعد تنفيذ الأوامر
 exit
