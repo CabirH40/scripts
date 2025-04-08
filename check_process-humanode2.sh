38b24dff5b843caaaea589344e10506a08a4fa35
cabir hasan <104315776+CabirH40@users.noreply.github.com>
2025-04-09 00:25:49 +0300

    Update check_process-humanode2.sh

--- check_process-humanode2.sh
+++ check_process-humanode2.sh
@@ -1,5 +1,8 @@
-wget https://github.com/CabirH40/script.sh/raw/main/check_process-humanode.py -O /root/check_process-humanode.py && chmod ug+x /root/check_process-humanode.py
-chmod +x /root/check_process-humanode.py
+# تحميل سكربت Python
+wget https://github.com/CabirH40/script.sh/raw/main/check_process-humanode.py -O /root/check_process-humanode.py && chmod +x /root/check_process-humanode.py
+
+# إنشاء ملف الخدمة
+cat <<EOF > /etc/systemd/system/check_process-humanode.service
 [Unit]
 Description=Humanode Process Checker
 After=network.target
@@ -12,8 +15,10 @@ User=root
 
 [Install]
 WantedBy=multi-user.target
+EOF
+
+# تفعيل وتشغيل الخدمة
 sudo systemctl daemon-reload
 sudo systemctl enable check_process-humanode.service
 sudo systemctl start check_process-humanode.service
-sudo systemctl restart check_process-humanode.service
 sudo systemctl status check_process-humanode.service
